3dadfd53beb75f0367dd6a8006239874fb2fff90
MNNoboa <48736616+MNNoboa@users.noreply.github.com>
2019-06-07 14:23:09 -0500

    Add files via upload

---
+++ gamble/pages.py
@@ -0,0 +1,32 @@
+from ._builtin import Page
+from random import randint
+
+
+class GambleInstructions(Page):
+    def vars_for_template(self):
+        return{
+            'erpoint': self.session.config['real_world_currency_per_point'] * 100
+        }
+
+
+class Gamble(Page):
+    form_model = 'player'
+    form_fields = ['gamble_number']
+
+    def before_next_page(self):
+        event = randint(0, 100)
+        if event <= 50:
+            self.player.set_gain('A')
+        else:
+            self.player.set_gain('B')
+
+
+class GambleResults(Page):
+    form_model = 'player'
+
+
+page_sequence = [
+    GambleInstructions,
+    Gamble,
+    GambleResults
+]
