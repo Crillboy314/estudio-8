b212eeef6153f5604782eda8970829d1873a30a6
Crillboy314 <48526278+Crillboy314@users.noreply.github.com>
2019-06-17 10:40:48 -0500

    Volviendo a añadir campos de preguntas

--- survey/models.py
+++ survey/models.py
@@ -17,21 +17,21 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
-#    age = models.IntegerField(
-#        label='¿Qué edad tiene usted?',
-#        min=13, max=125)
+    age = models.IntegerField(
+       label='¿Qué edad tiene usted?',
+      min=13, max=125)
 
-#   gender = models.StringField(
-#        choices=['Masculino', 'Femenino', 'Otro'],
-#        label='¿Qué es su género de usted?',
-#        widget=widgets.RadioSelect)
+    gender = models.StringField(
+        choices=['Masculino', 'Femenino', 'Otro'],
+        label='¿Qué es su género de usted?',
+        widget=widgets.RadioSelect)
 
-#    education = models.StringField(
-#        choices=['Ningún','Educación Inicial (Jardín)','Educación General Básica (Primaria)','Bachillerato (Secundaria)',
-#        'Algunos estudios universitarios','Instituto','Licenciatura o equivalente',
-#        'Maestría o equivalente','Doctorado'],
-#        label='¿Cuál es su nivel educativo?',
-#        widget=widgets.RadioSelect)
+    education = models.StringField(
+       choices=['Ningún','Educación Inicial (Jardín)','Educación General Básica (Primaria)','Bachillerato (Secundaria)',
+        'Algunos estudios universitarios','Instituto','Licenciatura o equivalente',
+        'Maestría o equivalente','Doctorado'],
+        label='¿Cuál es su nivel educativo?',
+        widget=widgets.RadioSelect)
 
     intent = models.StringField(
         choices=['Interesado','Generoso','Hostil','Cooperativo','Racional','Irracional'],
