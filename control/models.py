5aab6207d7b041d9a99df9257200f6f4747a4673
MNNoboa <48736616+MNNoboa@users.noreply.github.com>
2019-08-28 04:21:26 -0500

    Add files via upload

--- control/models.py
+++ control/models.py
@@ -1,6 +1,6 @@
 from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
-    Currency as c, currency_range
+    Currency as c
 )
 
 
@@ -11,19 +11,15 @@ payoffs to each of the participants.
 """
 
 
-
 class Constants(BaseConstants):
     name_in_url = 'control'
     players_per_group = 2
-    num_rounds = 2
+    num_rounds = 1
 
-    instructions_template = 'control/Instructions.html'
-
-    #Payoffs depending on the situation
+    # Payoffs depending on the situation
 
     YouA_OpponentB_payoff = c(70)
     YouB_OpponentA_payoff = c(20)
-
 
     both_B_payoff = c(40)
     both_A_payoff = c(10)
@@ -44,8 +40,6 @@ class Player(BasePlayer):
         widget=widgets.RadioSelect
     )
 
-    trial_payoff = models.CurrencyField(initial=0)
-
     def other_player(self):
         return self.get_others_in_group()[0]
 
@@ -64,10 +58,7 @@ class Player(BasePlayer):
                 }
         }
 
-        if self.round_number == 1:
-            self.trial_payoff = payoff_matrix[self.decision][self.other_player().decision]
-        else:
-            self.payoff = payoff_matrix[self.decision][self.other_player().decision]
+        self.payoff = payoff_matrix[self.decision][self.other_player().decision]
 
     question_1 = models.IntegerField(
         label = "Suponga que usted es la Primera Persona, y que selecciona B, ¿cuál sería su pago si la Segunda Persona también elige B?",
--- control/pages.py
+++ control/pages.py
@@ -1,43 +1,58 @@
 from ._builtin import Page, WaitPage
-from otree.api import Currency as c, currency_range
-from .models import Constants
+
+# class Introduction(Page):
+#     def vars_for_template(self):
+#         return {
+#             'sufee' : self.session.config['participation_fee'],
+#             'erpoint' : self.session.config['real_world_currency_per_point']*100
+#         }
+#
+#     def is_displayed(self):
+#         return self.round_number == 1
+#
+#
+# class Tree(Page):
+#
+#     def is_displayed(self):
+#         return self.round_number == 1
+#
+#
+# class Quiz(Page):
+#     form_model = 'player'
+#     form_fields = ['question_1', 'question_2']
+#
+#     def is_displayed(self):
+#         return self.round_number == 1
+#
+#     def error_message(self, values):
+#         if values['question_1'] != 40 and values['question_2'] != 20:
+#             return 'Both questions are incorrect'
+#         elif values['question_1'] == 40 and values['question_2'] !=20:
+#             return 'Question 2 is incorrect'
+#         elif values['question_1'] != 40 and values['question_2'] == 20:
+#             return 'Question 1 is incorrect'
 
 
-class Introduction(Page):
-    def vars_for_template(self):
-        return {
-            'sufee' : self.session.config['participation_fee'],
-            'erpoint' : self.session.config['real_world_currency_per_point']*100
-        }
-
-    def is_displayed(self):
-        return self.round_number == 1
+class Wait(WaitPage):
+    pass
 
 
-class Tree(Page):
-
-    def is_displayed(self):
-        return self.round_number == 1
-
-
-class Quiz(Page):
-    form_model = 'player'
-    form_fields = ['question_1', 'question_2']
-
-    def is_displayed(self):
-        return self.round_number == 1
-
-    def error_message(self, values):
-        if values['question_1'] != 40 and values['question_2'] != 20:
-            return 'Both questions are incorrect'
-        elif values['question_1'] == 40 and values['question_2'] !=20:
-            return 'Question 2 is incorrect'
-        elif values['question_1'] != 40 and values['question_2'] == 20:
-            return 'Question 1 is incorrect'
-
-class Decision(Page):
+class DecisionP1(Page):
+    template_name = 'control/Decision.html'
     form_model = 'player'
     form_fields = ['decision']
+
+    def is_displayed(self):
+        return self.player.id_in_group == 1
+
+
+class DecisionP2(Page):
+    template_name = 'control/Decision.html'
+    form_model = 'player'
+    form_fields = ['decision']
+
+    def is_displayed(self):
+        return self.player.id_in_group == 2
 
 
 class ResultsWaitPage(WaitPage):
@@ -50,7 +65,6 @@ class Results(Page):
     def vars_for_template(self):
         me = self.player
         opponent = me.other_player()
-        fee = self.session.config['participation_fee']
         return {
             'my_decision': me.decision,
             'opponent_decision': opponent.decision,
@@ -58,15 +72,16 @@ class Results(Page):
         }
 
     def app_after_this_page(self, upcoming_apps):
-        if self.round_number == 2:
-            return 'gamble'
+        return 'gamble'
 
 
 page_sequence = [
-    Introduction,
-    Tree,
-    Quiz,
-    Decision,
+    # Introduction,
+    # Tree,
+    # Quiz,
+    DecisionP1,
+    Wait,
+    DecisionP2,
     ResultsWaitPage,
     Results
 ]
